48c2ce66f9e456a0c2e59f7e7a2352f33e9e9d9f
WKent01 <kentwoolner@gmail.com>
2021-03-29 01:08:37 -0500

    final tic-tac-toe

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -1,14 +1,23 @@
 var gameOver = false;
 var totalMove = 0;
 var currentPlayer = 'X';
+const positions = {}
+positions["one"] = 1;
+positions[1] = "one";
+positions[positions[2] = "two"] = "2"
+positions[positions[3] = "three"] = "3"
+positions[positions[4] = "four"] = "4"
+positions[positions[5] = "five"] = "5"
+positions[positions[6] = "six"] = "6"
+positions[positions[7] = "seven"] = "7"
+positions[positions[8] = "eight"] = "8"
+positions[positions[9] = "nine"] = "9"
 
 
+function placeMarker(tile){
+    // get the spot by spotId
+    var currentTile = document.getElementById(positions[tile]);
 
-function placeMarker(spotID){
-    if(!gameOver){
-        //get the spot by spotId
-        var spot = document.getElementById(spotId);
-    }
     //get the spot/element by spotID
     //if the spot is empty
         //place the marker by changing the innerHTML of the spot
@@ -20,37 +29,120 @@ function placeMarker(spotID){
         //do nothing
     //else (the game is over)
         //do nothing
+
+    if(gameOver){
+        updateGameStatus();
+    }else{
+        //console.log(currentTile.innerHTML.length)
+        if(currentTile.innerHTML.length === 1){
+            return;
+        }else{
+            currentTile.innerHTML = currentPlayer;
+            totalMove++;
+            checkColumns();
+            checkRows();
+            checkDiagonals();
+            if(gameOver){
+                updateGameStatus();
+            }else{
+                if(currentPlayer === 'X'){
+                    currentPlayer = 'O';
+                }else{
+                    currentPlayer = 'X';
+                }
+                updateGameStatus();
+            }
+        }
+    }
+
+    
 }
 
 function currentPlayerWon(){
-    var s1 = doucment.getElementById('1');
-    //...
-    //var s9 = docuemnt.getElementById('9');
-    //get each spot using getElementById()
 
-    //for example, s1 = document.getElementById('1')'
-    //if
-    //(s1.innerHTML === s2.innerHTML && s2.innerHTML === s3.innerHTML) <= first row
-    //  || (s4.innerHTML === s5.innerHTML && s5.innerHTML === s6.innerHTML) <= second row
-    //...
-    //  gameOver = true;
-    //else
-    //  gameOver = false;
-    //
-    //if(!gameOver)
-    //  if(totalMove == 8) <== maybe 9
-    //      gameOver = true;
-    //return gameOver
+    return gameOver;
+}
+
+function checkRows(){
+    for(var i = 0; i < 3; i++){
+        t1 = document.getElementById(positions[1+3*i]);
+        t2 = document.getElementById(positions[2+3*i]);
+        t3 = document.getElementById(positions[3+3*i]);
+
+        if(t1.innerHTML.length > 0 && t2.innerHTML.length > 0 && t3.innerHTML.length > 0){
+            if(t1.innerHTML === t2.innerHTML && t2.innerHTML === t3.innerHTML){
+                gameOver = true;
+            }
+        }
+    }
+}
+
+function checkColumns(){
+    for(var i = 0; i < 3; i++){
+        t1 = document.getElementById(positions[1+i]);
+        t2 = document.getElementById(positions[4+i]);
+        t3 = document.getElementById(positions[7+i]);
+
+        if(t1.innerHTML.length > 0 && t2.innerHTML.length > 0 && t3.innerHTML.length > 0){
+            if(t1.innerHTML === t2.innerHTML && t2.innerHTML === t3.innerHTML){
+                gameOver = true;
+            }
+        }
+    }
+}
+
+function checkDiagonals(){
+    t1 = document.getElementById(positions[1]);
+    t3 = document.getElementById(positions[3]);
+    t5 = document.getElementById(positions[5]);
+    t7 = document.getElementById(positions[7]);
+    t9 = document.getElementById(positions[9]);
+
+    if(t1.innerHTML.length > 0 && t5.innerHTML.length > 0 && t9.innerHTML.length > 0){
+        if(t1.innerHTML === t5.innerHTML && t5.innerHTML === t9.innerHTML){
+            gameOver = true;
+        }
+    }
+
+    if(t3.innerHTML.length > 0 && t5.innerHTML.length > 0 && t7.innerHTML.length > 0){
+        if(t3.innerHTML === t5.innerHTML && t5.innerHTML === t7.innerHTML){
+            gameOver = true;
+        }
+    }
+
 }
 
 function updateGameStatus(){
-    var statusBoard = document.getElementById('status');
+    var gameStatus = document.getElementById('display');
     //if (gameOver){
     //  if it is not a tie, announce the winner
     // else, announce the winner
     //}else{
-    //  if nota tie ask for a move
+    //  if not a tie ask for a move
     // display the message
     //}
 
+    /*
+    if(gameOver){
+        if(totalMove == 9){
+            gameStatus.innerHTML = "The game resulted in a tie.";
+        }else{
+            gameStatus.innerHTML = "Player " + currentPlayer + " won.";
+        }
+    }else{
+        gameStatus.innerHTML = "Its Player " + currentPlayer + "'s turn.";
+    }
+    */
+
+    if(totalMove <= 3){
+        gameStatus.innerHTML = "Its Player " + currentPlayer + "'s turn.";
+    }else{
+        if(currentPlayerWon()){
+            gameStatus.innerHTML = "Player " + currentPlayer + " won.";
+        }else if(totalMove === 9 && !currentPlayerWon()){
+            gameStatus.innerHTML = "The game resulted in a tie.";
+        }else{
+            gameStatus.innerHTML = "Its Player " + currentPlayer + "'s turn.";
+        }
+    }
 }
